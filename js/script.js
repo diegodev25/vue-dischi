430867b08a0a5a2bebb99df52413df6e4e09e52c
Diego <sturbadiego25@outlook.it>
2020-12-01 15:17:05 +0100

    Struttura base html e css Vue Dischi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Facciamo una chiamata ajax all’api di boolean al seguente indirizzo.
+L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
+*/
+const api = 'https:flynn.boolean.careers/exercises/api/array/music';
+
+var app = new Vue ({
+  el: '#app',
+  data: {
+
+  }
+})
