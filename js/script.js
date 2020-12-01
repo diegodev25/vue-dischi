70be1218204271e39f3421c564261e0baab810e3
Diego <sturbadiego25@outlook.it>
2020-12-01 16:18:07 +0100

    Inserimento dati js per prendere i dati dall' API

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@
 Facciamo una chiamata ajax all’api di boolean al seguente indirizzo.
 L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
 */
-const api = 'https:flynn.boolean.careers/exercises/api/array/music';
+const genAlbum = 'https:flynn.boolean.careers/exercises/api/array/music';
 
 var app = new Vue ({
   el: '#app',
   data: {
-
+    album: null
+  },
+  mounted: function () {
+    axios.get(genAlbum)
+    .then(risposta => {
+      let dataAlbum = risposta.data.response;
+      this.album = dataAlbum;
+      console.log(this.album);
+    });
   }
 })
